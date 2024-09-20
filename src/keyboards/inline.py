a21213880e04cf21196ba6cd8e55f930add5aa12
alex <sashason1990@gmail.com>
2024-09-20 12:38:38 +0300

    Changed button

--- src/keyboards/inline.py
+++ src/keyboards/inline.py
@@ -1,11 +1,15 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
-def consult_keyboard(data_type=''):
-    text = '✅   Написать сообщение   🛍️✅'
+def consult_keyboard(data_type=""):
+    text = "✅   Получить со скидкой в 50%   🛍️✅"
     print(text)
-    consult_button = InlineKeyboardButton(text=text, callback_data="consult:" + data_type)
+    consult_button = InlineKeyboardButton(
+        text=text, callback_data="consult:" + data_type
+    )
     return InlineKeyboardMarkup().add(consult_button)
 
 
-handle_markup = InlineKeyboardMarkup().add(InlineKeyboardButton(text="ОБРАБОТАТЬ✅", callback_data="handle"))
+handle_markup = InlineKeyboardMarkup().add(
+    InlineKeyboardButton(text="ОБРАБОТАТЬ✅", callback_data="handle")
+)
